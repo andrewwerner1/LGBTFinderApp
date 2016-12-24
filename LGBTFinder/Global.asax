8250665550836aae0285aba41e5114839e063d39
andre_000 <andre_000@ANDY-ALLPURPOSE>
2016-12-24 16:11:15 -0500

    Add project files.

---
+++ LGBTFinder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LGBTFinder.MvcApplication" Language="C#" %>
---
+++ LGBTFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LGBTFinder.Startup))]
+namespace LGBTFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
